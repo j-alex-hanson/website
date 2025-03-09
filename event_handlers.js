7e9e72bf1eba9fd8e91b3d27e9eedcf769586f7b
Alex <hanson@cs.umd.edu>
2025-03-09 15:12:57 -0400

    Website Redesign + Adds Speedy-Splat

--- event_handlers.js
+++ event_handlers.js
@@ -1,7 +1,30 @@
 function start_hover(e, o) {
-  o.querySelector(".img_hover").style.opacity = "1";
+  // Don't propagate the event
+  e.stopPropagation();
+  
+  var hoverElement = o.querySelector(".teaser_hover");
+  hoverElement.style.opacity = "1";
+  
+  // If there's a video in the hover element, play it
+  var video = hoverElement.querySelector("video");
+  if (video) {
+    video.play();
+  }
 }
 
 function stop_hover(e, o) {
-  o.querySelector(".img_hover").style.opacity = "0";
+  // Don't propagate the event
+  e.stopPropagation();
+  
+  var hoverElement = o.querySelector(".teaser_hover");
+  hoverElement.style.opacity = "0";
+  
+  // If there's a video in the hover element, pause it
+  var video = hoverElement.querySelector("video");
+  if (video) {
+    // pasue video
+    //video.pause();
+    // reset video to start
+     video.currentTime = 0;
+  }
 }
